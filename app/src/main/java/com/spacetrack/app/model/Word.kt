a73a9baa6b315d9e092c289f89afead9a8233e04
Ronak <ronak.upadhyay@techgrains.com>
2019-09-16 19:25:08 +0530

    Flow changes done

---
+++ app/src/main/java/com/spacetrack/app/model/Word.kt
@@ -0,0 +1,5 @@
+package com.spacetrack.app.model
+
+import androidx.room.ColumnInfo
+
+data class Word( @ColumnInfo(name = "word") val word: String)
--- app/src/main/java/com/spacetrack/app/view/DashboardActivity.kt
+++ app/src/main/java/com/spacetrack/app/view/DashboardActivity.kt
@@ -1,6 +1,9 @@
 package com.spacetrack.app.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.spacetrack.app.R
 
@@ -10,8 +13,26 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        val btnAgencies = findViewById<LinearLayout>(R.id.btnAgencies)
+        val btnAstronaut = findViewById<LinearLayout>(R.id.btnAstronaut)
+        val btnSpacecraft = findViewById<LinearLayout>(R.id.btnSpacecraft)
+        val btnLauncher = findViewById<LinearLayout>(R.id.btnLauncher)
+        val btnLaunch = findViewById<LinearLayout>(R.id.btnLaunch)
+        val btnEvent = findViewById<LinearLayout>(R.id.btnEvent)
+
+        goToActivity("AgenciesActivity");
+
 
     }
 
+    fun goToActivity(Actvity : String){
+        var start = Intent(this, Actvity::class.java);
+        startActivity(start)
+    }
+
+
+    fun View.OnClickListener() {
+
+    }
 
 }
